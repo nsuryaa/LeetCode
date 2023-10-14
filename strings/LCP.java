caac94f9c933a4ca9c65bbfff1e79946120f8557
Surya <suryasedulous@gmail.com>
2023-10-14 06:08:20 +0530

    LCP

--- strings/LCP.java
+++ strings/LCP.java
@@ -14,4 +14,27 @@ class LCP {
         }
         return s1.substring(0, idx);
     }
+
 }
+
+
+
+
+//     public String longestCommonPrefix(String[] strs) {
+//         if (strs == null || strs.length == 0)
+//             return "";
+        
+//         Arrays.sort(strs);
+//         String first = strs[0];
+//         String last = strs[strs.length - 1];
+//         int c = 0;
+//         while(c < first.length())
+//         {
+//             if (first.charAt(c) == last.charAt(c))
+//                 c++;
+//             else
+//                 break;
+//         }
+//         return c == 0 ? "" : first.substring(0, c);
+//     }
+// }
